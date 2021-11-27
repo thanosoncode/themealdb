8c1e192793d787cc98c7ec9e1104f347ce5df720
Thanos Athens <thanosoncode@gmail.com>
2021-11-27 12:19:29 +0200

    testing

--- src/pages/MealDetails.js
+++ src/pages/MealDetails.js
@@ -52,95 +52,98 @@ const MealDetails = () => {
   if (loading) {
     return <Loading />;
   }
+  if (meal) {
+    const ingredients = Object.entries(meal)
+      .filter(
+        (entry) => entry[0].startsWith("strIngredient") && entry[1] !== ""
+      )
+      .map((entry) => entry[1]);
 
-  const ingredients = Object.entries(meal)
-    .filter((entry) => entry[0].startsWith("strIngredient") && entry[1] !== "")
-    .map((entry) => entry[1]);
+    const measures = Object.entries(meal)
+      .filter((entry) => entry[0].startsWith("strMeasure") && entry[1] !== " ")
+      .map((entry) => entry[1]);
 
-  const measures = Object.entries(meal)
-    .filter((entry) => entry[0].startsWith("strMeasure") && entry[1] !== " ")
-    .map((entry) => entry[1]);
+    let recipe = [];
 
-  let recipe = [];
+    ingredients.forEach((ing, i) => {
+      let array = [ing, measures[i]];
+      recipe.push(array);
+      return recipe;
+    });
 
-  ingredients.forEach((ing, i) => {
-    let array = [ing, measures[i]];
-    recipe.push(array);
-    return recipe;
-  });
+    const { strMeal, strMealThumb } = meal;
 
-  const { strMeal, strMealThumb } = meal;
-
-  return (
-    <>
-      <div className="details-content">
-        {loading && <Loading />}
-        <section className="details">
-          <div className="left">
-            <h3>{strMeal}</h3>
-            <img src={strMealThumb} alt={strMeal} />
-            <div className="prevNext">
-              <img
-                src="https://www.themealdb.com/images/icons/Arrow-Left.png"
-                alt=""
-                onClick={prevId}
-              />
-              <img
-                src="https://www.themealdb.com/images/icons/Arrow-Right.png"
-                alt=""
-                onClick={nextId}
-              />
+    return (
+      <>
+        <div className="details-content">
+          {loading && <Loading />}
+          <section className="details">
+            <div className="left">
+              <h3>{strMeal}</h3>
+              <img src={strMealThumb} alt={strMeal} />
+              <div className="prevNext">
+                <img
+                  src="https://www.themealdb.com/images/icons/Arrow-Left.png"
+                  alt=""
+                  onClick={prevId}
+                />
+                <img
+                  src="https://www.themealdb.com/images/icons/Arrow-Right.png"
+                  alt=""
+                  onClick={nextId}
+                />
+              </div>
             </div>
-          </div>
-          <div className="right">
-            <h3>Ingredients</h3>
-            <table className="table">
-              <tbody>
-                {recipe.map((item, index) => {
-                  return (
-                    <tr key={index}>
-                      <td
-                        style={{
-                          textAlign: "left",
-                          color: "var(--primary-light)",
-                          fontWeight: "600",
-                          paddingRight: "5rem",
-                        }}
-                      >
-                        {item[0]}
-                      </td>
-                      <td
-                        style={{
-                          textAlign: "right",
-                          color: "var(--primary)",
-                        }}
-                      >
-                        {item[1]}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </section>
-        <section className="instructions">
-          <h3>Instructions</h3>
-          <span>
-            {meal.strInstructions.slice(0, 300)}
-            <button
-              className="read-more-btn"
-              onClick={() => setReadMore(!readMore)}
-            >
-              {readMore ? "" : "read more"}
-            </button>
-            {readMore && <span>{meal.strInstructions.slice(300)}</span>}
-          </span>
-        </section>
-        <Footer />
-      </div>
-    </>
-  );
+            <div className="right">
+              <h3>Ingredients</h3>
+              <table className="table">
+                <tbody>
+                  {recipe.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td
+                          style={{
+                            textAlign: "left",
+                            color: "var(--primary-light)",
+                            fontWeight: "600",
+                            paddingRight: "5rem",
+                          }}
+                        >
+                          {item[0]}
+                        </td>
+                        <td
+                          style={{
+                            textAlign: "right",
+                            color: "var(--primary)",
+                          }}
+                        >
+                          {item[1]}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </section>
+          <section className="instructions">
+            <h3>Instructions</h3>
+            <span>
+              {meal.strInstructions.slice(0, 300)}
+              <button
+                className="read-more-btn"
+                onClick={() => setReadMore(!readMore)}
+              >
+                {readMore ? "" : "read more"}
+              </button>
+              {readMore && <span>{meal.strInstructions.slice(300)}</span>}
+            </span>
+          </section>
+          <Footer />
+        </div>
+      </>
+    );
+  }
 };
 
 export default MealDetails;
